84a634a372951de0d42181e273cd8d20827692d0
Hyeonguk Roh <hyeonguk.roh@gmail.com>
2023-09-09 14:13:55 -0400

    HelloWorld and Variables
    Began to learn fundamentals
    - printing hello world
    - learning variables

---
+++ Variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char character_name[] = "Tom";
+    int character_age = 1000;
+
+    printf("There was once a man named %s.\n", character_name);
+    printf("He was %d years old.\n", character_age);
+
+    character_age = 10000;
+    printf("He really liked the name %s.\n", character_name);
+    printf("But did not like being %d.\n", character_age);
+}
