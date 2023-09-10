71f26351d124618478d1eb150bc3d54376928e1e
Hyeonguk Roh <hyeonguk.roh@gmail.com>
2023-09-10 14:35:08 -0400

    Arrays

---
+++ 9_Arrays.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int lucky_numbers[] = {4, 8, 15, 16, 23, 42}; // [] means array or storing multiple pieces of information
+    int number[10];                               // an array of size 10
+    return 0;
+}
