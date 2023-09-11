608e43d8381edff5d8f5bc2eeccc75d532e5980a
Hyeonguk Roh <hyeonguk.roh@gmail.com>
2023-09-11 14:47:32 -0400

    Read Files

--- 23_WritingFiles.c
+++ 23_WritingFiles.c
@@ -3,8 +3,12 @@
 
 int main()
 {
-    FILE *f_pointer = fopen("23_WritingFiles_subfile", "a"); // fopen(filename, filemode) filemodes: r(read), w(write), a(append)
-    fprintf(f_pointer, "\nLol");
+    char line[255];
+    FILE *f_pointer = fopen("23_WritingFiles_subfile", "r"); // fopen(filename, filemode) filemodes: r(read), w(write), a(append)
+    // fprintf(f_pointer, "Lol");
+    fgets(line, 255, f_pointer);
+    printf("%s", line);
+
     fclose(f_pointer);
 
     return 0;
